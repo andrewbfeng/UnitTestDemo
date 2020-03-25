1f590ec3ad62dc39e4c7c77586e2c67a09a09c39
andrewbfeng <andrew_feng_cn@hotmail.com>
2020-03-25 09:37:28 +0800

    init project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Business business = new Business();
+            Thread thread = new Thread(business.runningBusiness);
+            Console.WriteLine("Starting Business");
+            thread.Start();
+            Console.ReadKey();
+        }
+    }
+    public class Business
+    {
+        int quota = 100;
+        public Status businessStatus = Status.Open;
+        Factory factory;
+        iStore seller;
+#if debug
+        public Business(Factory factory, iStore seller){
+this.factory = factory;
+this.seller = seller;
+}
+        public int Quota { get => quota; set => quota = value; }
+        public Business() {
+            factory = new Factory();
+            seller = new ShoppingMall();
+        }
+#else
+        public Business()
+        {
+            factory = new Factory();
+            seller = new ShoppingMall();
+        }
+#endif
+        public void runningBusiness()
+        {
+            businessStatus = Status.init;
+            while (true)
+            {
+                if (seller.checkInventory() > 500)
+                {
+                    Console.WriteLine("Selling Goods: {0}", seller.checkInventory());
+                    Debug.WriteLine("[Debug]Selling Goods: {0}", seller.checkInventory());
+                    seller.sell();
+                    businessStatus = Status.Closed;
+                    Console.WriteLine("Goods sold out");
+                    Debug.WriteLine("[Debug]Goods sold out");
+                    return;
+                }
+                else
+                {
+                    if (factory.goods - quota <= 0)
+                    {
+                        Console.WriteLine("Produced goods: {0}", quota - factory.goods);
+                        Debug.WriteLine("[Debug]Produced goods: {0}", quota - factory.goods);
+                        factory.produce(quota - factory.goods);
+                    }
+                    seller.fetchProduct(factory.deliverGoods());
+                }
+            }
+        }
+        public enum Status
+        {
+            init = 0,
+            Open = 1,
+            Closed = 2,
+        }
+    }
+
+
+    public class Factory
+    {
+        public int goods;
+        public void produce(int produceQuota)
+        {
+            goods += produceQuota;
+        }
+        public int deliverGoods()
+        {
+            int deliveredGoods = goods;
+            goods = 0;
+            return deliveredGoods;
+        }
+    }
+
+    class ShoppingMall : iStore
+    {
+        public int goods = 0;
+
+        public int checkInventory()
+        {
+            return goods;
+        }
+
+        public void fetchProduct(int productedGood)
+        {
+            goods += productedGood;
+        }
+        public void sell() { goods = 0; }
+    }
+
+
+    public interface iStore
+    {
+        int checkInventory();
+        void fetchProduct(int productedQuota);
+        void sell();
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1Tests/BusinessTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class BusinessTests
+    {
+        [TestMethod()]
+        public void UnitTest01_storeGoodsGreaterThan500WillTriggerSellAction()
+        {
+            //Arrange
+            Factory factoryFake = new Factory();
+            iStore sellerMock = new SellerMock();
+            Business testObject = new Business(factoryFake, sellerMock);
+
+            //Act
+            testObject.runningBusiness();
+            
+
+            //Assert
+            Assert.IsTrue(((SellerMock)sellerMock).isCalled);
+            Assert.AreEqual(2, (int)testObject.businessStatus);
+
+        }
+
+        public class SellerMock : iStore
+        {
+            public int goods = 0;
+            public bool isCalled = false;
+
+            public int checkInventory()
+            {
+                return 501;
+            }
+
+            public void fetchProduct(int productedQuota)
+            {
+                goods += productedQuota;
+            }
+            public void sell() { isCalled = true; goods = 0; }
+        }
+    }
+}
